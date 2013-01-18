5d868508fa10b94a53def757cc77e130fc45e688
Christopher Stawarz <cstawarz@mit.edu>
2013-01-18 11:47:55 -0500

    Basic device open/close

--- FDHapticDevice/FDHapticDevice.cpp
+++ FDHapticDevice/FDHapticDevice.cpp
@@ -8,6 +8,8 @@
 
 #include "FDHapticDevice.h"
 
+#include "dhdc.h"
+
 
 void FDHapticDevice::describeComponent(ComponentInfo &info) {
     IODevice::describeComponent(info);
@@ -16,10 +18,71 @@ void FDHapticDevice::describeComponent(ComponentInfo &info) {
 
 
 FDHapticDevice::FDHapticDevice(const ParameterValueMap &parameters) :
-    IODevice(parameters)
+    IODevice(parameters),
+    deviceID(NO_DEVICE)
 {
 }
 
 
 FDHapticDevice::~FDHapticDevice() {
+    if (deviceID != NO_DEVICE) {
+        if (dhdClose(deviceID) < 0) {
+            logDHDError("Unable to close haptic device");
+        }
+    }
 }
+
+
+bool FDHapticDevice::initialize() {
+    int result = dhdOpen();
+    
+    if (result < 0) {
+        logDHDError("Unable to open haptic device");
+        return false;
+    }
+    
+    deviceID = char(result);
+    
+    return true;
+}
+
+
+bool FDHapticDevice::startDeviceIO() {
+    return true;
+}
+
+
+bool FDHapticDevice::stopDeviceIO() {
+    return true;
+}
+
+
+void FDHapticDevice::logDHDError(const std::string &msg) {
+    merror(M_IODEVICE_MESSAGE_DOMAIN, "%s: %s", msg.c_str(), dhdErrorGetLastStr());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FDHapticDevice/FDHapticDevice.h
+++ FDHapticDevice/FDHapticDevice.h
@@ -9,6 +9,11 @@
 #ifndef __FDHapticDevice__FDHapticDevice__
 #define __FDHapticDevice__FDHapticDevice__
 
+#include <string>
+
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_signed.hpp>
+
 using namespace mw;
 
 
@@ -20,7 +25,45 @@ public:
     explicit FDHapticDevice(const ParameterValueMap &parameters);
     ~FDHapticDevice();
     
+    bool initialize() MW_OVERRIDE;
+    bool startDeviceIO() MW_OVERRIDE;
+    bool stopDeviceIO() MW_OVERRIDE;
+    
+private:
+    BOOST_STATIC_ASSERT(boost::is_signed<char>::value);
+    static const char NO_DEVICE = -1;
+    
+    static void logDHDError(const std::string &msg);
+    
+    char deviceID;
+    
 };
 
 
 #endif // !defined(__FDHapticDevice__FDHapticDevice__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
